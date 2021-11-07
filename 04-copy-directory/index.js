f3add71196481e18306d1d77185171f215d1e06f
divakotova <divakova.dasha@yandex.by>
2021-11-07 17:27:25 +0300

    feat: add folder cleanup

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,24 +2,21 @@ const fs = require('fs');
 const fsPromises = fs.promises;
 const path = require('path');
 
-fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }).then(function() {
-    console.log('Directory created successfully');
-}).catch(function() {
-    console.log('failed to create directory');
-});
+async function copyDirectory() {
+    await fsPromises.rmdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    await fsPromises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true });
+    await fsPromises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
+    .then((files) => {
+        for (let file of files) {
+            fsPromises.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`))
+    .then(function() {
+      console.log(`File ${file.name} Copied`);
+    })  
+        }
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
 
-fs.promises.readdir(path.join(__dirname, 'files'), {withFileTypes: true})
-.then((files) => {
-    for (let file of files) {
-        fsPromises.copyFile(path.join(__dirname, 'files', `${file.name}`), path.join(__dirname, 'files-copy', `${file.name}`))
-.then(function() {
-  console.log(`File ${file.name} Copied`);
-})
-.catch(function(error) {
-  console.log(error);
-});      
-    }
-})
-.catch(error => {
-    console.log(error)
-})
+copyDirectory()
